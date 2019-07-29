c62b494f988cba02937a095a2ab4bafda11be4e4
Нурмухаметов Даниил Эдуардович <nurmukhametovde@pik-industry.ru>
2019-07-29 17:04:01 +0300

    string3 2

--- Quest/src/String_3/Str_3.java
+++ Quest/src/String_3/Str_3.java
@@ -6,27 +6,47 @@ import java.util.regex.Pattern;
 
 public class Str_3 {
 
-    public static void countYZ (String s){
+    public static void countYZ(String s) {
         Pattern pattern;
         Matcher matcher;
         int i = 0;
         pattern = Pattern.compile("\\b([a-z]|[A-Z])*[zyZY]\\b");
         matcher = pattern.matcher(s);
-        while (matcher.find()){
-            i+=1;
+        while (matcher.find()) {
+            i += 1;
         }
         System.out.println(i);
     }
 
-    public static void withoutString (String a, String b){
+    public static void withoutString(String a, String b) {
         ArrayList<String> slova = new ArrayList<>();
         int k = 0;
-        for (int i=0; i<a.length();i++){
-            if (a.charAt(i)==' ') {
-                slova.add(a.substring(k,i));
-                k = i+1;
+        int dlina = b.length();
+        for (int i = 0; i < a.length(); i++) {
+            if (!Character.isLetter(a.charAt(i))) {
+                slova.add(a.substring(k, i + 1));
+                k = i + 1;
+            } else if (i == a.length() - 1) {
+                slova.add(a.substring(k));
             }
         }
-        for (String x: slova) System.out.println(x);
+        for (int i = 0; i < slova.size(); i++) {
+            if (slova.get(i).indexOf(b)>0) {
+                slova.set(i, slova.get(i).substring(0, slova.get(i).indexOf(b))
+                        .concat(slova.get(i).substring(slova.get(i).indexOf(b) + dlina)));
+            }
+            else if (slova.get(i).indexOf(b)==0){
+                slova.set(i, slova.get(i).substring(b.length()));
+            }
+        }
+        String v = slova.get(0);
+        for (int i=0; i<slova.size()-1; i++){
+            v = v.concat(slova.get(i+1));
+        }
+        System.out.println(v);
+
+//        for (String X : slova) System.out.println(X);
+        }
+
     }
-}
+
--- Quest/src/String_3/Str_3_Demo.java
+++ Quest/src/String_3/Str_3_Demo.java
@@ -3,7 +3,6 @@ package String_3;
 public class Str_3_Demo {
     public static void main(String[] args) {
 //        Str_3.countYZ("ZXY cdz");
-        Str_3.withoutString("xyf get zg","fge");
-
+        Str_3.withoutString("Hello tellhere ellephant","ell");
     }
 }
