fc639c17667a9d9a7c121ebbc3111bb69f1dafde
Нурмухаметов Даниил Эдуардович <nurmukhametovde@pik-industry.ru>
2019-10-23 17:26:25 +0300

    tbot2

--- Robot/src/Bot.java
+++ Robot/src/Bot.java
@@ -1,26 +1,96 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 
 public class Bot extends TelegramLongPollingBot {
 
-    public void onUpdateReceived(Update update){
+    //ИД чата
+    private long chat_id;
+
+    //клавиатура
+    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+    //исходник для включения
+    Path SourceOn = Paths.get("C:\\Users\\nurmukhametovde\\IdeaProjects\\Robot\\SourceFiles\\On.txt");
+    //исходник для отключения
+    Path SourceOff = Paths.get("C:\\Users\\nurmukhametovde\\IdeaProjects\\Robot\\SourceFiles\\Off.txt");
+    //управляющая директория
+    Path Destination = Paths.get("C:\\Users\\nurmukhametovde\\IdeaProjects\\Robot\\RequiredFolder\\Status.txt");
+
+
+    public void onUpdateReceived(Update update) {
         update.getUpdateId();
-
         SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        chat_id = update.getMessage().getChatId();
 
-        if(update.getMessage().getText().equals("On")){
-            sendMessage.setText("hello");
-            try{
-                execute(sendMessage);
-            }
-            catch (TelegramApiException e){
-                e.printStackTrace();
-            }
+        String text = update.getMessage().getText();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        try{
+            sendMessage.setText(getMessage(text));
+            execute(sendMessage);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
         }
-
     }
+
+    public String getMessage(String msg){
+        //список кнопок
+        ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+        //количество рядов копок
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        //настройка некоторых параметров клавиатуры
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        //очистка клавиатуры
+        keyboard.clear();
+        keyboardFirstRow.clear();
+        //добавление кнопок
+        keyboardFirstRow.add("On");
+        keyboardFirstRow.add("Off");
+        keyboard.add(keyboardFirstRow);
+
+        String answ;
+        switch (msg){
+            //включение перерасчета
+            case "on":
+                try {
+                    Files.copy(SourceOn, Destination, StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                answ = "Перерасчет включен";
+                break;
+
+            //отключение перерасчета
+            case "off":
+                try {
+                    Files.copy(SourceOff, Destination, StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                answ = "Перерасчет отключен";
+                break;
+
+            default:
+                answ = "Введите команду";
+                break;
+        }
+        return answ;
+    }
+
 
     public String getBotUsername(){
         return "@recalculator_bot";
--- Robot/src/CopyFiles.java
+++ Robot/src/CopyFiles.java
@@ -1,26 +1,54 @@
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.Scanner;
 
 public class CopyFiles {
     public static void main(String[] args) {
-        String instruction = "off";
+        String instruction = "";
+        String status = "";
         Path pathSourceOn = Paths.get("C:\\Users\\nurmukhametovde\\IdeaProjects\\Robot\\SourceFiles\\On.txt");
         Path pathSourceOff = Paths.get("C:\\Users\\nurmukhametovde\\IdeaProjects\\Robot\\SourceFiles\\Off.txt");
         Path pathDestination = Paths.get("C:\\Users\\nurmukhametovde\\IdeaProjects\\Robot\\RequiredFolder\\Status.txt");
+
+        System.out.println("Set instruction");
+
+        for(;;){
+        Scanner vvod = new Scanner(System.in);
+        instruction = vvod.nextLine();
+
         switch (instruction) {
             case "on":
+                if (status.equals("on")) {
+                    System.out.println("Rec is on already");
+                    break;
+                }
                 try {
                     Files.copy(pathSourceOn, pathDestination, StandardCopyOption.REPLACE_EXISTING);
+                    status = instruction;
+                    System.out.println("Rec is on");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                break;
 
             case "off":
+                if (status.equals("off")) {
+                    System.out.println("Rec is off already");
+                    break;
+                }
                 try {
                     Files.copy(pathSourceOff, pathDestination, StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e){
+                    status = instruction;
+                    System.out.println("Rec is off");
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
+                break;
+
+            default:
+                System.out.println("Set instruction");
+        }
+
     }
     }
 }
